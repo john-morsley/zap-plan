065ac543d160e62eb29d35f70fa297df38bc9382
John Morsley <johnmorsley@gmail.com>
2025-12-02 16:21:50 +0000

    Renamed some files and added some console like writes to the script files.

--- zap/api-auth.js
+++ zap/api-auth.js
@@ -1,4 +1,6 @@
 function authenticate(helper, paramsValues, credentials) {
+  print("[AUTH] Starting authentication...");
+  
   var HttpRequestHeader = Java.type('org.parosproxy.paros.network.HttpRequestHeader');
   var HttpHeader = Java.type('org.parosproxy.paros.network.HttpHeader');
   var URI = Java.type('org.apache.commons.httpclient.URI');
@@ -7,8 +9,12 @@ function authenticate(helper, paramsValues, credentials) {
   var clientId = paramsValues.get("client_id");
   var clientSecret = paramsValues.get("client_secret");
   
+  print("[AUTH] Endpoint: " + authEndpoint);
+  print("[AUTH] Client ID: " + clientId);
+  
   // Build URL with query parameters
   var authUrl = authEndpoint + "?id=" + clientId + "&secret=" + clientSecret;
+  print("[AUTH] Full URL: " + authUrl);
   
   // Create POST request
   var msg = helper.prepareMessage();
@@ -21,7 +27,11 @@ function authenticate(helper, paramsValues, credentials) {
   msg.setRequestHeader(requestHeader);
   
   // Send the request
+  print("[AUTH] Sending authentication request...");
   helper.sendAndReceive(msg);
+  
+  print("[AUTH] Response status: " + msg.getResponseHeader().getStatusCode());
+  print("[AUTH] Response body: " + msg.getResponseBody().toString());
   
   return msg;
 }
